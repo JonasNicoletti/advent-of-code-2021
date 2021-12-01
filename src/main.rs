8e0502ac86eb8e87104844d308f678d7a29a0d87
Sjnao <53706359+JonasNicoletti@users.noreply.github.com>
2021-12-01 17:03:08 +0000

    (feat) execute only one day

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
+use std::collections::HashMap;
 mod day1;
+mod day2;
 
 fn main() {
-  day1::part1();
-  day1::part2();
+  use clap::{load_yaml, App};
+
+  let yaml = load_yaml!("cli.yml");
+  let matches = App::from_yaml(yaml).get_matches();
+
+  let day = matches.value_of("day").unwrap_or("all");
+  let mut days: HashMap<&str, fn() -> ()> =
+    HashMap::from([("1", day1::main), ("2", day2::main)]);
+
+  if day == "all" {
+    for (day, day_fn) in days {
+      println!("Running day {}", day);
+      day_fn();
+    }
+  } else {
+    println!("Running day {}", day);
+    days.get(day).unwrap_or(unknown_day)();
+  }
+}
+fn unknown_day() {
+  println!("Unknown day");
 }
