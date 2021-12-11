dc201c4e31635fadb75c3c5262c98732d9ed9e0e
Sjnao <53706359+JonasNicoletti@users.noreply.github.com>
2021-12-11 06:24:58 +0000

    clean up day 11

--- src/day11.py
+++ src/day11.py
@@ -1,4 +1,3 @@
-flashes = 0
 with open("src/inputs/day11.txt") as f:
     octopuses = [list(map(int, list(line.strip()))) for line in f]
 
@@ -10,20 +9,19 @@ total_octopuses = len(octopuses)*len(octopuses)
 
 
 def flash(i, j, octopuses):
-    global flashes
-
+    flashes = 0
     octopuses[i][j] += 1
     if octopuses[i][j] == 10:
         flashes += 1
         for x, y in adjacents:
             try:
                 if i+x >= 0 and j+y >= 0:
-                    flash(i+x, j+y, octopuses)
+                    flashes += flash(i+x, j+y, octopuses)[1]
             except IndexError:
 
                 pass
 
-    return octopuses
+    return octopuses, flashes
 
 
 # for day in range(100):
@@ -33,7 +31,9 @@ while True:
     flashes = 0
     for i, row in enumerate(octopuses):
         for j, octopus in enumerate(row):
-            octopuses = flash(i, j, octopuses)
+            f = flash(i, j, octopuses)
+            octopuses = f[0]
+            flashes += f[1]
 
     print(f"Day {day}: {flashes} out of {total_octopuses}")
     octopuses = [[0 if octopus > 9 else octopus for octopus in row]
